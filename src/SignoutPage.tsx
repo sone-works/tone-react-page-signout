8e5cdd1f7c93f5c23e64ee1c26b8e171950feca0
Lon Beshiri <lonbeshiri@gmail.com>
2023-12-24 09:39:51 -0500

    fix: wording

--- src/SignoutPage.tsx
+++ src/SignoutPage.tsx
@@ -2,24 +2,31 @@ import ToneServiceApi from '@sone-dao/tone-react-api'
 import { UseUserStore } from '@sone-dao/tone-react-user-store'
 import { deleteCookie } from 'cookies-next'
 import { useRouter } from 'next/navigation'
-import { useEffect } from 'react'
-import { useCountdown } from 'usehooks-ts'
+import { useEffect, useState } from 'react'
+import { useCountdown, useInterval } from 'usehooks-ts'
 
 type SignoutPageProps = {
   useUserStore: UseUserStore
 }
 
 export default function SignoutPage({ useUserStore }: SignoutPageProps) {
+  const [handFlip, setHandFlip] = useState<boolean>(false)
   const [count, { startCountdown }] = useCountdown({
     countStart: 5,
     intervalMs: 1000,
   })
+
+  useInterval(() => {
+    setHandFlip(!handFlip)
+  }, 500)
 
   const api = new ToneServiceApi()
 
   const router = useRouter()
 
   useEffect(() => {
+    !localStorage.getItem('tone.session') && router.push('/')
+
     startCountdown()
   }, [])
 
@@ -27,20 +34,26 @@ export default function SignoutPage({ useUserStore }: SignoutPageProps) {
 
   return (
     <main className="flex items-center justify-center bg-global min-h-screen h-full p-4">
-      <div className="p-4 rounded-xl bg-global-flipped text-global-flipped flex flex-col items-center w-full">
-        <i className="fa-solid fa-hand-wave text-8xl" />
-        <p className="mt-4 p-2 font-content">Thanks for stopping by!</p>
-        <p className="mt-2">
-          Signing you out{' '}
-          {count ? (
-            <>
-              in {count} second
-              {count > 1 && 's'}...
-            </>
-          ) : (
-            <>now...</>
-          )}
-        </p>
+      <div className="flex flex-col items-center w-full max-w-xl">
+        <span className="font-release text-global text-5xl m-4">tone</span>
+        <div className="p-4 rounded-xl bg-global-flipped text-global-flipped flex flex-col items-center w-full">
+          <i
+            className="fa-solid fa-hand-wave text-8xl"
+            style={{ transform: handFlip ? 'scaleX(-1)' : '' }}
+          />
+          <p className="mt-4 p-2 font-content">Thanks for stopping by!</p>
+          <p className="mt-2">
+            We're signing you out{' '}
+            {count ? (
+              <>
+                in {count} second
+                {count > 1 && 's'}...
+              </>
+            ) : (
+              <>now...</>
+            )}
+          </p>
+        </div>
       </div>
     </main>
   )
@@ -49,8 +62,6 @@ export default function SignoutPage({ useUserStore }: SignoutPageProps) {
     return api.user
       .signoutUser()
       .then((response) => {
-        console.log({ response })
-
         localStorage.removeItem('tone.session')
 
         useUserStore.setState({}, true)
@@ -59,8 +70,6 @@ export default function SignoutPage({ useUserStore }: SignoutPageProps) {
 
         router.push('/')
       })
-      .catch((error) => {
-        console.log({ error })
-      })
+      .catch((error) => {})
   }
 }
